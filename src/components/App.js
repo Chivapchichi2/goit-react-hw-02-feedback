1d05b68151488e86b0e8632b0cae2191f42576a3
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-05-28 18:42:00 +0300

    step 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+    <>
       <header className="App-header">
         <h1>react hw-02 feedback</h1>
       </header>
-    </div>
+      <main>
+          <section>
+            <h2>Please leave feedback</h2>
+            <button type="button">Good</button>
+            <button type="button">Neutral</button>
+            <button type="button">Bad</button>
+          </section>
+          <section>
+            <h2>Statistics</h2>
+            <p>Good: { good }</p>
+            <p>Neutral: { neutral }</p>
+            <p>Bad: { bad }</p>
+          </section>
+      </main>
+    </>
   );
+  }
 }
 
 export default App;
